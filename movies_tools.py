5958f602faa52e60cb3aad43557c9164cd3ce0ea
Kim Vuong <kvuong@umich.edu>
2019-02-19 19:42:24 -0500

    finished movies_tools file

--- movies_tools.py
+++ movies_tools.py
@@ -21,24 +21,11 @@ for row in row_reader:
     movie_rating_lst.append(row)
 
 row_count = sum(1 for row in movie_rating_lst)
-print(row_count)
+# print(row_count)
 
-
-for movie in movie_rating_lst:
+movie_insts = []
+for movie in movie_rating_lst[:10]:
     m = Movie(movie)
+    movie_insts.append(m)
 
-# print(m)
-
-
-
-# define a class Movie that accepts as constructor input one row of the movies_clean.csv file (in whatever format you think is appropriate for you).
-#
-# That class should have any instance variables and/or method(s) you think will be useful for building your Flask app, and at least one of your Flask app's routes should create instances of that class.
-#
-# We suggest that you make this plan based on your goals for the Flask app (detailed below), and go back and forth between the two Python files making plans as you write code (similarly to how you might have gone back and forth between your lab3_code.py and your SI507_project1.py code to work on Project 1!). We expect you to use instances of this class in the Flask app.
-#
-# It is also possible that you will choose to define additional function/s in movies_tools.py and invoke them in the Flask code, but it is not necessary to do so. You can make your own design decisions here.
-#
-# We also suggest that, as you work, you have, beneath an if __name__ == "__main__": statement, code that opens up the clean movies data file and uses its contents to test out some instances of this class -- in order to ensure it works as you want it to. It's more annoying to debug inside Flask than it is to debug code outside Flask first, so read what your goals are, make sure your class works, and THEN integrate it into the app!
-#
-#
+# print(movie_insts)
